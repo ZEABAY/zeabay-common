343d9845ee2aa5e58149786e6eb2191fc030097a
Zeynel Abiddin Aydar <zeynelaydar@gmail.com>
2026-02-08 02:08:35 +0300

    test(hello-service): cover traceparent precedence and canonicalization

--- zeabay-common-spring-boot-starter/src/main/java/com/zeabay/common/tsid/TsidIdGenerator.java
+++ zeabay-common-spring-boot-starter/src/main/java/com/zeabay/common/tsid/TsidIdGenerator.java
@@ -1,10 +1,11 @@
 package com.zeabay.common.tsid;
 
 import com.github.f4b6a3.tsid.TsidCreator;
+import java.util.Locale;
 
 public final class TsidIdGenerator {
 
   public String newId() {
-    return TsidCreator.getTsid().toString();
+    return TsidCreator.getTsid().toString().toLowerCase(Locale.ROOT);
   }
 }
