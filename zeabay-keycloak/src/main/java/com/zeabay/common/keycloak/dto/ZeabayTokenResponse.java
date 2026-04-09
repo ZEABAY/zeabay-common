b74dbe0fbcd209953e2cad713930373cf5cdf8b5
Zeynel Abiddin Aydar <zeynelaydar@gmail.com>
2026-04-09 20:49:58 +0300

    Feat: add refresh token expires in

--- zeabay-keycloak/src/main/java/com/zeabay/common/keycloak/dto/ZeabayTokenResponse.java
+++ zeabay-keycloak/src/main/java/com/zeabay/common/keycloak/dto/ZeabayTokenResponse.java
@@ -5,4 +5,5 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public record ZeabayTokenResponse(
     @JsonProperty("access_token") String accessToken,
     @JsonProperty("refresh_token") String refreshToken,
-    @JsonProperty("expires_in") Integer expiresIn) {}
+    @JsonProperty("expires_in") Integer expiresIn,
+    @JsonProperty("refresh_expires_in") Integer refreshExpiresIn) {}
